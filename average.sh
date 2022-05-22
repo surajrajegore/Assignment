25b52c3a517cdc6e97f5afe9452fc3ada1327806
surajrajegore <rajegoresuraj5555@gmail.com>
2022-05-22 12:26:33 +0530

    Added :code of reads 5 Random 2 Digit values , then find their sum and the average

---
+++ average.sh
@@ -0,0 +1,18 @@
+num1=$((10+$RANDOM %91))
+num2=$((10+$RANDOM %91))
+num3=$((10+$RANDOM %91))
+num4=$((10+$RANDOM %91))
+num5=$((10+$RANDOM %91))
+
+echo "value of num1 is "$num1
+echo "value of num1 is "$num1
+echo "value of num1 is "$num1
+echo "value of num1 is "$num1
+echo "value of num1 is "$num1
+
+sum=$((num1+num2+num3+num4+num5))
+echo "sum of five numbers is "$sum
+
+average=$((sum/5))
+
+echo "Average of five numbers is "$average

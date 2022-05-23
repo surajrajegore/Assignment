d60f4416477d74df46806a036c3dd5891f030661
surajrajegore <rajegoresuraj5555@gmail.com>
2022-05-23 08:03:27 +0530

    Added

---
+++ days.sh
@@ -0,0 +1,26 @@
+echo "Enter number you wamt to enter "
+read num
+if((num==1))
+then
+        echo "sunday"
+elif((num==2))
+then
+        echo "monday"
+elif((num==3))
+then
+        echo "tuesday"
+elif((num==4))
+then
+        echo "wednesday"
+elif((num==5))
+then
+        echo "thursday"
+elif((num==6))
+then
+        echo "friday"
+elif((num==7))
+then
+        echo "saturday"
+else
+        echo "wrong input"
+fi

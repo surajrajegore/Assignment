70a9a533f5ea24fe4b268807108c9f48334926d6
surajrajegore <rajegoresuraj5555@gmail.com>
2022-05-23 07:42:04 +0530

    Added:Read a single digit number and write the number in word

---
+++ numberdigit.sh
@@ -0,0 +1,32 @@
+echo "Enter number you wamt to enter "
+read num
+if((num==0))
+then
+	echo "zero"
+elif((num==1))
+then
+	echo "one"
+elif((num==3))
+then
+	echo "two"
+elif((num==4))
+then
+	echo "four"
+elif((num==5))
+then
+	echo "five"
+elif((num==6))
+then
+	echo "six"
+elif((num==7))
+then
+	echo "seven"
+elif((num==8))
+then
+	echo "eight"
+elif((num==9))
+then
+	echo "nine"
+else
+	echo "num is greater than 9"
+fi

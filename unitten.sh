a51387a70664b7fc602d8b5fee4969eac41bf238
surajrajegore <rajegoresuraj5555@gmail.com>
2022-05-23 08:11:20 +0530

    added:unit digits

---
+++ unitten.sh
@@ -0,0 +1,18 @@
+echo "Enter number you wamt to enter "
+read num
+if((num==1))
+then
+        echo "unit"
+elif((num==10))
+then
+        echo "ten"
+elif((num==100))
+then
+        echo "hundred"
+elif((num==1000))
+then
+        echo "thousand"
+else
+        echo "wrong input"
+fi
+

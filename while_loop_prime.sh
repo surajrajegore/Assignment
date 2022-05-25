277e29802c20f1ae2acf6ca2e39efd70a490aed5
surajrajegore <rajegoresuraj5555@gmail.com>
2022-05-25 14:33:40 +0530

    Added:added code of power of two using while

---
+++ while_loop_prime.sh
@@ -0,0 +1,17 @@
+num=2
+echo "Enter the nth power "
+read n
+power=1
+i=0
+while((i<=n))
+do
+        if((power==256))
+        then
+                break
+        else
+                echo "$i" " $power"
+                power=$(( power * num   ))
+                ((i++))
+        fi
+
+done
